01294b3f80671f53863150864d42d319373e539a
tatasauce4life <tatasauce4life@gmail.com>
2026-04-13 07:04:12 +0000

    first time submit

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Flask Web App Starter
+Please convert the RED apple into a BLUE one, by using HSI/HSV transform.
 
-A Flask starter template as per [these docs](https://flask.palletsprojects.com/en/3.0.x/quickstart/#a-minimal-application).
+請利用HSI或HSV的轉換，更換color space之後，將附檔圖片內的紅蘋果(紅色色調區域)，轉變成藍蘋果(藍色色調)。
 
-## Getting Started
-
-Previews should run automatically when starting a workspace.
+Channel-swapping is not allowed! 
+(不可以利用channel-swapping來交作業，但是可以用channel-swapping來體會紅蘋果變藍蘋果的樣子。)
--- main.py
+++ main.py
@@ -1,15 +1,141 @@
-import os
+# HSV的轉換，更換color space之後，將附檔圖片內的紅蘋果(紅色色調區域)，轉變成藍蘋果(藍色色調)。
 
-from flask import Flask, send_file
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return send_file('src/index.html')
+class apple():
+    def __init__(self,path:str):
+        self.path = path
+        self.piexl = None
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+        self.R = None
+        self.G = None
+        self.B = None
 
-if __name__ == "__main__":
-    main()
+        self.H = None
+        self.S = None
+        self.V = None
+    
+    # load_img()
+    def load_img(self):
+        '''
+        func: load png
+        '''
+        bgr_img = cv2.imread(self.path)
+        rgb_img = cv2.cvtColor(bgr_img,cv2.COLOR_BGR2RGB)
+        self.piexl = rgb_img
+        
+        # 3 channels to R G B
+        self.R = rgb_img[:,:,0]
+        self.G = rgb_img[:,:,1]
+        self.B = rgb_img[:,:,2]
+
+
+
+    # RGB2HSV()
+    def RGB2HSV(self):
+        '''
+        func: change rgb space to hsv space
+        Args:
+            R, G, B:numpy array of pixel in rgb form
+        Return:
+            H, S, V:numpy array of pixel in hsv form
+        '''
+        C_high = np.max(self.R,self.G,self.B)
+        C_low = np.min(self.R,self.G,self.B)
+        C_rng = C_high - C_low
+
+        self.V = C_high
+        self.S = C_rng / C_high
+
+        # 1. normalization of R G B
+        R_ = (self.R - C_low) / C_rng
+        G_ = (self.G - C_low) / C_rng
+        B_ = (self.B - C_low) / C_rng
+
+        # 2. set H'
+        if C_high == self.R:
+            self.H = B_ - G_
+        elif C_high == self.G:
+            self.H = 2 + R_ - B_
+        elif C_high == self.B:
+            self.H = 4 + G_ - R_
+        else:
+            print("something went wrong")
+        
+        # 3. scale to [0,1)
+        if self.H <0:
+            self.H += 6
+            self.H /= 6
+        else:
+            self.H /= 6
+        
+        # 4. project to circle
+        self.H *= 360
+        
+
+    # HSV2RGB()
+    def HSV2RGB(self):
+        '''
+        func: change hsv space to rgb space
+        Args:
+            H, S, V:numpy array of pixel in rgb form
+        Return:
+            R, G, B:numpy array of pixel in hsv form
+        '''
+        # 1. check where the point is in the hexagon
+        H = self.H/360
+        H_ = np.mod(6*H,6)
+        c1 = np.floor(H_) #c1 = 0,1,2,3,4,5
+        c2 = H_ - c1
+        
+        # 2. set the RGB value tone
+        v = self.V # RGB 中的最大值
+        x = (1-self.S) * v # RGB 中的最小值
+        z = (1-(1-c2)*self.S) * v # 比例上升
+        y = (1 - c2*self.S) * v # 比例下降
+
+        '''
+        switch (c1) {
+         case 0: r = V; g = z; b = x; break;
+         case 1: r = y; g = V; b = x; break;
+         case 2: r = x; g = V; b = z; break;
+         case 3: r = x; g = y; b = V; break;
+         case 4: r = z; g = x; b = V; break;
+         case 5: r = V; g = x; b = y; break;
+         }
+        '''
+        # 3. distribute the value to RGB
+        if c1 == 0:
+            self.R, self.G, self.B = v, z, x
+        elif c1 ==1:
+            self.R, self.G, self.B = y, v, x
+        elif c1 ==2:
+            self.R, self.G, self.B = x, v, z
+        elif c1 ==3:
+            self.R, self.G, self.B = x, y, v
+        elif c1 ==4:
+            self.R, self.G, self.B = z, x, v
+        elif c1 ==5:
+            self.R, self.G, self.B = v, x, y
+        
+        # 4. scale to 0~255
+        self.B = min(255*self.B,255)
+        self.G = min(255*self.G,255)
+        self.R = min(255*self.R,255)
+
+    # HSV_shift()
+    def HSV_shift(self,switch_mode:str):
+        '''
+        func: change the color by shifting the hue value
+        Args:
+            switch = ['R2B','R2G']
+            'R2B': turn red and blue
+            'R2G': turn red and green
+        '''
+        if switch_mode == 'R2B':
+            self.H += 240
+        elif switch_mode == 'R2G':
+            self.H += 120
